219155164c74f3d92d6a5a7b8a3a3c5aef87125f
roistaff <roistaff1983@gmail.com>
2024-05-16 20:21:01 +0900

    in develop

--- main.go
+++ main.go
@@ -1,5 +1,13 @@
 package main
-import "github.com/roistaff/groq-go-client/client"
+import (
+	"github.com/roistaff/groq-go-client/client"
+	"os"
+	"context"
+)
 func main(){
-	client.Print()
+	groq,err := client.New("https://api.groq.com/openai/v1/chat/completions",os.Getenv("GROQ_API_KEY"), nil)
+	if err != nil{
+		panic(err)
+	}
 }
+items,err := Client.Chat(context.Background(),)
